1a041b15c09999f7f67006c39597436078db45de
Alexander G. M. Smith <agmsmith@ncf.ca>
2018-12-14 19:03:35 +0000

    Add a full_title helper to generate page titles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the full page title generated on a per-page basis, with defaults.
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample Application"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
