5bc1e82eb7e373e8a026acbc433d3076963de8ce
Alexander G. M. Smith <agmsmith@ncf.ca>
2018-12-17 20:45:40 +0000

    Added checks for name and e-mail not being empty, using validation system.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "Stock user instance created for testing should pass validity test" do
+    assert @user.valid?
+  end
+
+  test "name should be present and not blank or empty" do
+    @user.name = "     "
+    assert_not @user.valid?
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present and not blank or empty" do
+    @user.email = "     "
+    assert_not @user.valid?
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
 end
