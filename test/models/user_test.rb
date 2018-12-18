1b2a108819cf0624c85bd0c39d6b33ae91d08fe0
Alexander G. M. Smith <agmsmith@ncf.ca>
2018-12-18 23:00:03 +0000

    Add validation of e-mail (regex, length), and test uniqueness too.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,4 +23,45 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    # Maximum database string size is often 255 characters.  So we want to
+    # reject e-mails that are longer than that.
+    @user.email = "a" * 244 + "@example.com"
+    assert @user.email.length >= 256
+    assert_not @user.valid?
+  end
+
+  test "email should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+    # Should be case insensitive duplicate rejection too.
+    duplicate_user.email = duplicate_user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
