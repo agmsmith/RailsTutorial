10b6711ad447daab114cfa0a7d3c4e2210f500c8
Alexander G. M. Smith <agmsmith@ncf.ca>
2018-12-28 15:28:54 -0500

    Add a Gravatar icon for the user.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # Returns the Gravatar picture HTML for the given user.
+  def gravatar_for(user, size: 80)
+    gravatar_id = Digest::MD5::hexdigest(user.email.squish.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
