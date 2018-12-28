16841d4de8880eccc61140e10240dbea90b7278d
Alexander G. M. Smith <agmsmith@ncf.ca>
2018-12-28 19:18:03 +0000

    Add password to User data access.  Add user resource, and display user page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+
+  resources :users
+  # So /users shows all, /users/1 shows user with ID 1, /users/new makes one,
+  # /usr/1/edit, POST /Users, PATCH and DELETE /users/1 do stuff, etc.
 end
