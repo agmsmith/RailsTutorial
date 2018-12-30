0918fbc069f0e7903edc4723403fa2069389dc6a
Alexander G. M. Smith <agmsmith@ncf.ca>
2018-12-30 15:17:14 -0500

    Add a FORM for signups and error display when you get it wrong.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def new
+  def new # First part of making a new user, show a web form.
+    @user = User.new
   end
+
+  def create # Second part of making a new user, store stuff from web form.
+    @user = User.new(user_params) # Get cleaned up user input parameters.
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
