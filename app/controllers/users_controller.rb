c277b0673c34dbcaa057181c419e683174ce8e60
Alexander G. M. Smith <agmsmith@ncf.ca>
2018-12-31 17:01:46 -0500

    Add a Flash message to welcome new users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,10 @@ class UsersController < ApplicationController
   def create # Second part of making a new user, store stuff from web form.
     @user = User.new(user_params) # Get cleaned up user input parameters.
     if @user.save
-      # Handle a successful save.
+      # flash shows up in rendering the following page, hidden in session.
+      flash[:success] = "Welcome to the Sample Application, #{@user.name}!"
+      # Handle a successful save, new user created!  Show their profile page.
+      redirect_to @user # Equivalent to user_url(@user), ends up as /users/123
     else
       render 'new'
     end
@@ -18,7 +21,7 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
+    def user_params # Filter out unwanted parameters, from web form.
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
