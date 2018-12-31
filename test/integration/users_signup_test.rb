d6414a892efeed8053475e63dcb17b577bd78742
Alexander G. M. Smith <agmsmith@ncf.ca>
2018-12-31 16:02:46 -0500

    Adding tests of the sign-up page, just invalid user ones.

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup doesn't create new user" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "foo",
+          password_confirmation: "bar"
+        }
+      }
+    end
+    assert_template 'users/new'
+  end
+
+  test "empty signup has enough error messages" do
+    get signup_path
+    post users_path, params: { user: { name: "  " }}
+    N = 6
+    # Verify that there are N error messages.
+    assert_select "div#error_explanation" do
+      # assert_select "div.alert" do |alertdiv| p alertdiv.inner_text.squish end
+      assert_select "div.alert", {count: 1, text: "The form contains #{N} errors."}
+      assert_select "ul" do
+        assert_select "li", count: N
+      end
+    end
+  end
+
+end
