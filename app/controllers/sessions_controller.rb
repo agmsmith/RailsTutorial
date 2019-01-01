43e652e05e8f1ea0353a877c7aca83550fcc7af2
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-01-01 17:52:07 -0500

    Check password in login page and show a message for failed logins.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log in the user and redirect to the user's show page.
+    else # Wrong password or bad e-mail address.
+      # Only show the flash on the next page, not on subsequent ones.
+      flash.now[:danger] = 'Invalid email & password combination.'
+      render 'new'
+    end
   end
 
   def destroy
