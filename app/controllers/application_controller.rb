e11f4d7e5c253e194d58dc25d0f8096f43694392
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-01-03 14:47:47 -0500

    Add log-in functionality using a session cookie.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include SessionsHelper
+  include SessionsHelper # So all controllers can use login sessions.
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log in the user and redirect to the user's show page.
+      log_in user # Adds a session cookie with user's ID number.
+      redirect_to user # Show the user's profile page.
     else # Wrong password or bad e-mail address.
       # Only show the flash on the next page, not on subsequent ones.
       flash.now[:danger] = 'Invalid email & password combination.'
