365a11ee5f8a0bcedbaba1cc42904a8e76cb5e84
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-01-04 16:53:21 -0500

    Make a test user via Fixtures and test that login works.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael) # Get it from the Fixture/test database users.yml.
+  end
+
   test "login with empty information" do
     get login_path
     assert_template 'sessions/new'
@@ -10,6 +14,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?, "Flash should be empty on second following page."
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    assert flash.empty?, "Flash should be empty after successful login."
   end
 
 end
