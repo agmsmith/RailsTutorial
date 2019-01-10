3b0c19e36416e564d80a0b6f15652b3dfc3542ce
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-01-10 15:30:44 -0500

    Rearrange code by functionality.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,13 +6,31 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   # Remembers a user in a persistent session.
   def remember(user)
-    user.remember
+    user.remember # Makes remember_token and adds digest to Users database.
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Forgets a persistent session (deletes long term cookies, database token).
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Get & cache the currently logged in user (does permanent login too), or nil.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -23,23 +41,6 @@ module SessionsHelper
         @current_user = user
       end
     end
-  end
-
-  def logged_in?
-    !current_user.nil?
-  end
-
-  # Forgets a persistent session (deletes long term cookies, database token).
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
-
-  def log_out
-    forget(current_user)
-    session.delete(:user_id)
-    @current_user = nil
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -32,15 +32,15 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # Forget the user (remove token from database).
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-
-  # Forget the user (remove token from database).
-  def forget
-    update_attribute(:remember_digest, nil)
   end
 
 end
