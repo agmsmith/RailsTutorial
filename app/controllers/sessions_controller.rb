e0cbd75dadd0c6d0ee98930efe11a453205ba965
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-01-10 14:52:32 -0500

    Add a permanent cookie to make logins persistent across browser sessions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log in the user and redirect to the user's show page.
       log_in user # Adds a session cookie with user's ID number.
+      remember user # Add a permanent cookie to skip future logins.
       redirect_to user # Show the user's profile page.
     else # Wrong password or bad e-mail address.
       # Only show the flash on the next page, not on subsequent ones.
@@ -17,8 +18,12 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
-    flash[:notice] = "You have been logged out."
+    if logged_in?
+      log_out
+      flash[:notice] = "You have been logged out."
+    else
+      flash[:warning] = "You have already logged out elsewhere!  Now double logged out :-)"
+    end
     redirect_to root_url
   end
 
