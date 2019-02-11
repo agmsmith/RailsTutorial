ac67f4bd15a16e1e255a8ae50f57003ba961517d
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-02-11 11:42:29 -0500

    Add tests for deleting users.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,6 +12,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should be able to see an individual user's profile when not logged in" do
+    get user_path(@user)
+    assert_select "title", full_title("Showing: " + @user.name)
+  end
+
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?, "visiting the edit page without a login should show an error"
@@ -50,6 +55,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "shouldn't show index of all users when not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
   end
 
 end
