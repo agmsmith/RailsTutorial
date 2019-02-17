21fd3e42841cf9abca1ade30e6d33af26dcf28cc
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-02-17 17:24:04 -0500

    Adding real mail server settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,16 +66,25 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Use :test for internal testing, other protocol symbols for real use.
-  config.action_mailer.delivery_method = :test
-
   # Really should use http/https depending on their web site connection.
   config.action_mailer.default_url_options = {
     host: 'www.agmsmith.ca', protocol: 'https' }
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # Set this to true and configure the email server for immediate delivery
+  # to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
+
+  # Use :test for internal testing, :smtp for normal mail server.
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => '465',
+    :authentication => :plain,
+    :user_name => 'agmsrepsys@gmail.com',
+    :password => 'insert-real-password-here',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
