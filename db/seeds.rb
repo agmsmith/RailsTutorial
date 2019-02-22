54d7d31fdb085ce85be5d115c4edcad70d365d23
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-02-22 17:54:42 -0500

    CSS for microposts, though it doesn't quite work right in wide windows.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 # Note that create! (bang version) raises an exception on invalid data.
 
+puts "Creating 100 users..."
+
 User.create!(name: "Example User",
   email: "example@railstutorial.org",
   password: "foobar",
@@ -26,4 +28,12 @@ User.create!(name: "Example User",
     password_confirmation: password,
     activated: true,
     activated_at: Time.zone.now)
+end
+
+puts "Creating microposts for some of the users..."
+
+users = User.order(:created_at).take(6)
+50.times do |n|
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
