e0384bc5d3e515e87ecbef0e82d813d1bcfc765d
Alexander G. M. Smith <agmsmith@ncf.ca>
2019-03-05 17:13:25 -0500

    Tested deletion of Microposts and found related picture deleted too, even on S3.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user # Has a user_id field that identifies our owner.
   default_scope -> { order(created_at: :desc) } # Sort by newest date first.
-  mount_uploader :picture, PictureUploader
+  mount_uploader :picture, PictureUploader # Hooks in functionality, like deleting picture when micropost is deleted.
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate :picture_size
